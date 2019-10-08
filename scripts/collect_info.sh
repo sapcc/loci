10f7556824d9d88e52ab8801da97c1aa3b54f928
Vasyl Saienko <vsaienko@mirantis.com>
2019-10-08 09:24:38 +0300

    Add collect_info.sh stage
    Collect info about installed packages versions and
    pip packages into /etc/image_info folder
    
    Change-Id: I781457ceeb821a0cad7edc9c0921736b989e9ec7
    (Adapted for our cloud)

---
+++ scripts/collect_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+INFO_DIR="/etc/image_info"
+mkdir -p $INFO_DIR
+PACKAGES_INFO="${INFO_DIR}/packages.txt"
+PIP_INFO="${INFO_DIR}/pip.txt"
+
+case ${distro} in
+    debian|ubuntu)
+        dpkg -l > $PACKAGES_INFO
+        ;;
+    *)
+        echo "Unknown distro: ${distro}"
+        exit 1
+        ;;
+esac
+
+pip freeze > $PIP_INFO
