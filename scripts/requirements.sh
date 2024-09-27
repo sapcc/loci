f2671ca71d08e7139ee82eb980a9de5d8d945807
Fabian Wiesel <fabian.wiesel@sap.com>
2024-09-27 15:58:27 +0200

    Align Dockerfile and script more with upstream
    The KEEP_ALL_WHEELS default is set in the build-arg.
    
    Change-Id: I7992c394a98008e8c5f1accf27cc165ff5d9859b

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -95,7 +95,7 @@ export CPUCOUNT=1
 # Build all dependencies in parallel. This is safe because we are
 # constrained on the version and we are building with --no-deps
 echo uwsgi enum-compat ${PIP_PACKAGES} | xargs -n1 | split -l1 -a3
-if [[ "${KEEP_ALL_WHEELS:=False}" == "False" ]]; then
+if [[ "$KEEP_ALL_WHEELS" == "False" ]]; then
   ls -1 | xargs -n1 -P20 -t bash -c 'pip wheel ${PIP_WHEEL_ARGS} --find-links /source-wheels --find-links / --no-deps --wheel-dir / -c /upper-constraints.txt -r $1 || cat $1 >> /failure' _ | tee /tmp/wheels.txt
   # Remove native-binary wheels, we only want to keep wheels that we
   # compiled ourselves.
