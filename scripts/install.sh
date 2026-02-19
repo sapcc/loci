209b45c2e05dc112615eed99a8f8441a99fb5829
Johannes Kulik <johannes.kulik@sap.com>
2026-02-19 13:03:44 +0100

    Revert "Add pyroscope as user-customization"
    This reverts commit 95e5a014fd84314192654841db8d1bbb22eefb65.
    
    There's no current plan to use pyroscope, so we remove it.

--- scripts/install.sh
+++ scripts/install.sh
@@ -118,5 +118,5 @@ else
         pip install --no-cache-dir --pre --no-compile -r /tmp/${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS}
     fi
 fi
-$(dirname $0)/setup_pyroscope.py
+
 $(dirname $0)/cleanup.sh
